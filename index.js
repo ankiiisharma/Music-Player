d6fbac87e4da6d79714e19b90e9ff22566776eb5
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-01 23:14:37 +0530

    Songs added

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Aaftaab = new Audio('./songs/Aaftaab.mp3');
+const WBV = new Audio('./songs/WBV.mp3');
+const KaiseyJiyun = new Audio('./songs/Kaisey_Jiyun.mp3');
+const DilMere = new Audio('./songs/Dil_Mere.mp3');
 
 
-Aaftaab.play()
+Aaftaab.play();
+WBV.play();
+KaiseyJiyun.play();
+DilMere.play();
